94cb309171ea2748ecb90930ed33d2c60c6a88fb
Rian <brianalmeida998@gmail.com>
2022-06-14 00:17:22 -0300

    Commit 06/14/22

--- Projeto-Bloco-I/src/br/com/generation/projeto/metodo03.java
+++ Projeto-Bloco-I/src/br/com/generation/projeto/metodo03.java
@@ -6,17 +6,17 @@ public class metodo03 {
 
 	public static void metodo03(String nome) {
 		
-		int opcao1;
+		int opcao2;
 		
 		JOptionPane.showMessageDialog(null, "Escolha as opção a seguir");
 
-		opcao1 = Integer.parseInt(JOptionPane.showInputDialog("1 - "));
+		opcao2 = Integer.parseInt(JOptionPane.showInputDialog("1 - Foi ao trabalho sem intercorrências.\n2 - No ônibus percebeu alguns olhares por conta de sua roupa e se sentiu desconfortável."));
 
-		JOptionPane.showMessageDialog(null, "");
+		JOptionPane.showMessageDialog(null, nome + " chegou na empresa e foi ao banheiro se trocar, avisando a todos que por conta de seus compromissos tinha acordado atrasada");
 		
-		opcao1 = Integer.parseInt(JOptionPane.showInputDialog(""));
+		opcao2 = Integer.parseInt(JOptionPane.showInputDialog("1 - Seus colegas de trabalho compreenderam o motivo, estão todos sobrecarregados, então imprevistos podem ocorrer.\n2 - Seus colegas de trabalho não acreditaram em suas palavras e acham que aquilo foi uma falta de comprometimento com as regras da empresa."));
 
-		JOptionPane.showMessageDialog(null, "");
+		JOptionPane.showMessageDialog(null, "Na reunião " + nome + " traz uma proposta para o projeto.");
 
 	}
 }
