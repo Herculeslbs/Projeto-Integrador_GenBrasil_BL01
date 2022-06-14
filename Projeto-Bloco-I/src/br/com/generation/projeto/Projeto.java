f1a8795d2d21742deec65a333d1c387ae99d9802
Rian <brianalmeida998@gmail.com>
2022-06-13 22:09:28 -0300

    Commit 06/13/22

--- Projeto-Bloco-I/src/br/com/generation/projeto/Projeto.java
+++ Projeto-Bloco-I/src/br/com/generation/projeto/Projeto.java
@@ -2,58 +2,40 @@ package br.com.generation.projeto;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class Projeto {
 
 	public static void main(String[] args) {
 
-		Scanner leitor = new Scanner(System.in);
-
-		Mulher m1 = new Mulher();
+		String m1 = null;
 
 		int opcao1, opcao2;
 
-		System.out.println("Digite o nome da mulher para iniciar o jogo interativo");
+		m1 = JOptionPane.showInputDialog("Digite o nome da Protagonista para iniciar o jogo interativo");
 
-		m1.setNome(leitor.next());
+		JOptionPane.showMessageDialog(null, m1 + " acordou para ir ao trabalho.");
 
-		System.out.println(m1.getNome() + " acordou para ir ao trabalho.");
-		System.out.println("Escolha a opção a seguir: ");
-		System.out.println("1 - Acordou na hora");
-		System.out.println("2 - Acordou atrasada");
-
-		opcao1 = leitor.nextInt();
+		opcao1 = Integer.parseInt(JOptionPane.showInputDialog("Agora vamos começar com as escolhas:\n1 - Acordou na hora | 2 - Acordou atrasada"));
 
 		if (opcao1 == 1) {
 			
-			System.out.println(
-					m1.getNome() + " dirige até o trabalho. No estacionamento acidentalmente encosta no carro de um "
-							+ "funcionário que estava chegando");
-			
-			System.out.println("Escolha a opção a seguir");
-			
-			System.out.println("1 - Os dois desceram do carro e se acertaram sobre o problema acontecido. "
-					+ m1.getNome() + " deu"
-					+ "seu número e se prontificou a chamar o seguro. O outro funcionário a tranquilizou: 'acidentes acontecem'");
-			
-			System.out.println("2 - O funcionário sai do carro bravo, ofendendo " + m1.getNome()
-					+ ". Soltou palavras como \'tinha que ser mulher para não saber dirigir'." + m1.getNome()
-					+ "sentiu medo por trabalhar no mesmo local que este homem, também se sentiu frustrada "
-					+ "por ter cometido esse deslize.");
-			
-			opcao2 = leitor.nextInt();
-
+			JOptionPane.showMessageDialog(null, m1 + " se arruma, sai de casa e dirige até o trabalho. No estacionamento acidentalmente encosta no carro de um funcionário que estava chegando");
 		}
 
 		else if (opcao1 == 2) {
-			System.out.println(
-					m1.getNome() + " ficou preocupada em se atrasar para o trabalho, então, colocou rapidamente um"
-							+ " shorts e uma blusa que encontrou e resolveu se trocar no trabalho.");
+			JOptionPane.showMessageDialog(null, m1 + " ficou preocupada em se atrasar para o trabalho, então, colocou rapidamente um"+ " shorts e uma blusa que encontrou e resolveu se trocar no trabalho.");
 		}
 
 		else {
 			System.out.println("Opção inválida");
 		}
 
+		JOptionPane.showMessageDialog(null, "Escolha a opção a seguir");
+		
+		opcao2 = Integer.parseInt(JOptionPane.showInputDialog("1 - Os dois desceram do carro e se acertaram sobre o problema acontecido, " + m1 + " deu" + " seu número e se prontificou a chamar o seguro.\n\n" + "O outro funcionário a tranquilizou dizendo: acidentes acontecem!\n\n" + "2 - O funcionário sai do carro bravo," + m1 + " Dizendo as seguintes palavra: tinha que ser mulher para não saber dirigir!\n\n" + m1 + " sentiu medo por trabalhar no mesmo local que este homem, também se sentiu frustrada " + "por ter cometido esse deslize."));
+		
+		
 	}
 
 }
---
+++ Projeto-Bloco-I/src/br/com/generation/projeto/metodo01.java
@@ -0,0 +1,7 @@
+package br.com.generation.projeto;
+
+public class metodo01 {
+
+	
+	
+}
