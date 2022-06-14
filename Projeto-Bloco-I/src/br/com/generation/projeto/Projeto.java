a4713ab8abc58f2c8ea305adc1ce14c0b7c193f3
Rian <brianalmeida998@gmail.com>
2022-06-14 00:39:44 -0300

    Commit 06/14/22

--- Projeto-Bloco-I/src/br/com/generation/projeto/Projeto.java
+++ Projeto-Bloco-I/src/br/com/generation/projeto/Projeto.java
@@ -2,37 +2,42 @@ package br.com.generation.projeto;
 
 import javax.swing.JOptionPane;
 
-public class Projeto{
+public class Projeto {
 
 	public static void main(String[] args) {
 
 		String m1;
 
 		int opcao1;
-		
-		JOptionPane.showMessageDialog(null, "Neste Projeto iremos apresentar um Game interativo vivendo o dia a dia de uma mulher desenvolvedora!");
 
-		JOptionPane.showMessageDialog(null, "O jogo é simples, tudo se baseia em escolhas, e cada escolha feita no jogo gera uma consequência, seja boa ou ruim!");
-		
+		JOptionPane.showMessageDialog(null,
+				"Neste Projeto iremos apresentar um Game interativo vivendo o dia a dia de uma mulher desenvolvedora!");
+
+		JOptionPane.showMessageDialog(null,
+				"O jogo é simples, tudo se baseia em escolhas, e cada escolha feita no jogo gera uma consequência, seja boa ou ruim!");
+
 		JOptionPane.showMessageDialog(null, "Bom, já que entendeu, vamos começar!");
-		
+
 		m1 = JOptionPane.showInputDialog("Digite o nome da Protagonista para iniciar o jogo interativo:");
 
 		JOptionPane.showMessageDialog(null, m1 + " Acordou para ir ao trabalho.");
 
-		opcao1 = Integer.parseInt(JOptionPane.showInputDialog("Agora vamos começar com as escolhas:\n1 - Acordou na hora | 2 - Acordou atrasada"));
+		opcao1 = Integer.parseInt(JOptionPane
+				.showInputDialog("Agora vamos começar com as escolhas:\n1 - Acordou na hora | 2 - Acordou atrasada"));
 
 		if (opcao1 == 1) {
-			
-			JOptionPane.showMessageDialog(null, m1 + " Se arruma, sai de casa e dirige até o trabalho. No estacionamento acidentalmente encosta no carro de um funcionário que estava chegando.");
-			
-			metodo01.metodo02(m1);
+
+			JOptionPane.showMessageDialog(null, m1
+					+ " Se arruma, sai de casa e dirige até o trabalho. No estacionamento acidentalmente encosta no carro de um funcionário que estava chegando.");
+
+			metodo01.metodo01(m1);
 		}
 
 		else if (opcao1 == 2) {
-			JOptionPane.showMessageDialog(null, m1 + " Ficou preocupada em se atrasar para o trabalho, então, colocou rapidamente um shorts e uma blusa que encontrou no armário, e levar a roupa para se trocar no local para evitar o atraso.");
-		
-			metodo02.metodo03(m1);
+			JOptionPane.showMessageDialog(null, m1
+					+ " Ficou preocupada em se atrasar para o trabalho, então, colocou rapidamente um shorts e uma blusa que encontrou no armário, e levar a roupa para se trocar no local para evitar o atraso.");
+
+			metodo02.metodo02(m1);
 		}
 
 		else {
@@ -40,5 +45,5 @@ public class Projeto{
 		}
 
 	}
-	
+
 }
--- Projeto-Bloco-I/src/br/com/generation/projeto/metodo01.java
+++ Projeto-Bloco-I/src/br/com/generation/projeto/metodo01.java
@@ -2,22 +2,53 @@ package br.com.generation.projeto;
 
 import javax.swing.JOptionPane;
 
-public class metodo01{
+public class metodo01 {
 
-	public static void metodo02(String nome) {
-		
+	public static void metodo01(String nome) {
+
 		int opcao1;
-		
-		JOptionPane.showMessageDialog(null, "Escolha as opção a seguir");
 
-		opcao1 = Integer.parseInt(JOptionPane.showInputDialog("1 - Os dois desceram do carro e se acertaram sobre o problema acontecido, " + nome + " deu" + " seu número e se prontificou a chamar o seguro.\n\n" + "O outro funcionário a tranquilizou dizendo: acidentes acontecem!\n\n" + "2 - O funcionário sai do carro bravo," + nome + " Dizendo as seguintes palavra: tinha que ser mulher para não saber dirigir!\n\n" + nome + " sentiu medo por trabalhar no mesmo local que este homem, também se sentiu frustrada por ter cometido esse deslize."));
+		JOptionPane.showMessageDialog(null, "Escolha as opções a seguir");
 
-		JOptionPane.showMessageDialog(null, "Por conta do ocorrido " + nome + " acabou chegando atrasada para uma reunião, explicou toda a situação e por fim se desculpou pelo atraso.");
-		
-		opcao1 = Integer.parseInt(JOptionPane.showInputDialog("1 - A equipe entendeu o ocorrido, perguntando se estava tudo bem, e a reunião prosseguiu normalmente.\n2 - " + nome + " ouviu piadas de mau gosto sobre o ocorrido, sentiu-se constrangida, mas como já havia se atrasado simplesmente sentou e aguardou a reunião prosseguir."));
+		opcao1 = Integer.parseInt(
+				JOptionPane.showInputDialog("1 - Os dois desceram do carro e se acertaram sobre o problema acontecido, "
+						+ nome + " deu" + " seu número e se prontificou a chamar o seguro.\n\n"
+						+ "O outro funcionário a tranquilizou dizendo: acidentes acontecem!\n\n"
+						+ "2 - O funcionário sai do carro bravo," + nome
+						+ " Dizendo as seguintes palavra: tinha que ser mulher para não saber dirigir!\n\n" + nome
+						+ " sentiu medo por trabalhar no mesmo local que este homem, também se sentiu frustrada por ter cometido esse deslize."));
 
-		JOptionPane.showMessageDialog(null, "Ainda um pouco estressada após o ocorrido no início do dia " + nome + " optou por comer sozinha no refeitório, não estava com muita vontade de socializar.");
+		JOptionPane.showMessageDialog(null, "Por conta do ocorrido " + nome
+				+ " acabou chegando atrasada para uma reunião, explicou toda a situação e por fim se desculpou pelo atraso.");
+
+		opcao1 = Integer.parseInt(JOptionPane.showInputDialog(
+				"1 - A equipe entendeu o ocorrido, perguntando se estava tudo bem, e a reunião prosseguiu normalmente.\n2 - "
+						+ nome
+						+ " ouviu piadas de mau gosto sobre o ocorrido, sentiu-se constrangida, mas como já havia se atrasado simplesmente sentou e aguardou a reunião prosseguir."));
+
+		JOptionPane.showMessageDialog(null, "Ainda um pouco estressada após o ocorrido no início do dia " + nome
+				+ " optou por comer sozinha no refeitório, não estava com muita vontade de socializar.");
 
 	}
 
+	public static void metodo02(String nome) {
+
+		int opcao1;
+
+		JOptionPane.showMessageDialog(null, "Escolha as opções a seguir");
+
+		opcao1 = Integer.parseInt(JOptionPane.showInputDialog("1 - " + nome
+				+ " almoçou tranquilamente. Seus colegas perguntaram se estava tudo bem e se precisava de alguma coisa.\n2 - Um colega de trabalho sentou-se ao seu lado, dizendo que "
+				+ nome
+				+ " era muito bonita para comer sozinha, e fez convites inadequados para o ambiente de trabalho."));
+
+		JOptionPane.showMessageDialog(null,
+				"Na reunião de equipe, o Senior avisa que tem um bug em algum código e sugere deixar essa demanda nas mãos do estagiário.");
+
+		opcao1 = Integer.parseInt(JOptionPane.showInputDialog("1 - Como " + nome
+				+ " era especialista em Java, ela se propôs a auxiliar o estagiário no Bug, fazendo com que ele aprendesse a lidar com essas situações.\n2 - Ela sugere uma solução, mas é interrompida rispidamente durante sua fala pelo seu Sênior."));
+
+		JOptionPane.showMessageDialog(null, "O gerente da a permissão para " + nome
+				+ ", e elogia sua proatividade, aumentando sua influencia na empresa.");
+	}
 }
