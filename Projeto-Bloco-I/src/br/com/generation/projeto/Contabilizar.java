586bfac6a039eece9bb0365ea5a81469181e99c9
Wanessa Koserski <wkoserski@gmail.com>
2022-06-14 16:09:59 -0300

    get and setter class Contabilizar

--- Projeto-Bloco-I/src/br/com/generation/projeto/Contabilizar.java
+++ Projeto-Bloco-I/src/br/com/generation/projeto/Contabilizar.java
@@ -2,9 +2,27 @@ package br.com.generation.projeto;
 
 public class Contabilizar {
 
-	int quantasEscolhas01 = 0;
-	int quantasEscolhas02 = 0;
+	private int quantasEscolhas01 = 0;
+	private int quantasEscolhas02 = 0;
 	
+	
+	
+	public int getQuantasEscolhas01() {
+		return quantasEscolhas01;
+	}
+
+	public void setQuantasEscolhas01(int quantasEscolhas01) {
+		this.quantasEscolhas01 = quantasEscolhas01;
+	}
+
+	public int getQuantasEscolhas02() {
+		return quantasEscolhas02;
+	}
+
+	public void setQuantasEscolhas02(int quantasEscolhas02) {
+		this.quantasEscolhas02 = quantasEscolhas02;
+	}
+
 	void somarEscolhas01() {
 		this.quantasEscolhas01++;
 	}
