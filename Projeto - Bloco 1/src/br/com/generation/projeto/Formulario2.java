37ffc5079241a29eca2993e53cf187e20e004c21
Andreia <deia_kas@hotmail.com>
2022-06-14 21:16:05 -0300

    adicionando alteração

--- Projeto - Bloco 1/src/br/com/generation/projeto/Formulario2.java
+++ Projeto - Bloco 1/src/br/com/generation/projeto/Formulario2.java
@@ -8,41 +8,47 @@ public class Formulario2 {
 		
 		
 		String opiniaoM, feedback;
-		int check1, check2;
-		boolean abuso,superior=false,salario=false;
+		int check1, check2, check3, check4;
+		boolean abuso,superior=false,maisHomens=false;
 		do {	
-	check1 = Integer.parseInt(JOptionPane.showInputDialog("Segundo o levantamento das empresas de pesquisa Nielsen e Opinion Box, \n"
-			+ "a maior parte das mulheres sabe o que é assédio (92%)\n e 46% delas já sofreram ou presenciaram isso no trabalho, \nespecialmente na faixa etária entre 24 e 45 anos.\r\n"
-			+ "\r\n"
-			+ "	Você já sofreu algum abuso no seu local de trabalho?\n\nDigite: \n\n(1) para sim \n\n(2) para não\n\n"));
+	check1 = Integer.parseInt(JOptionPane.showInputDialog("Você acredita que pode causar uma mudança no seu local de trabalho?\n\nDigite: \n\n(1) para sim \n\n(2) para não\n\n"));
 		}
 		while(check1 != 1 && check1 != 2);
 					
 	if(check1 == 1) {
 		abuso = true;
-		int sup =Integer.parseInt(JOptionPane.showInputDialog("Esse abuso veio de um superior?\n\nDigite: \n\n(1) para sim\n\n(2) para não\n\n"));
-		if (sup==1) {
-			superior=true;
+		
 		} else {
-			superior=false;
+			abuso=false;
 		}
-	} else {
-		abuso = false;
-	}
 	
 	do {
-	check2 = Integer.parseInt(JOptionPane.showInputDialog("De acordo com um estudo do IBGE as mulheres ganharam em média 20,50%"
-			+ "\nmenos do que os homens no 4º trimestre de 2021.\r\n"
-			+ "\r\n"
-			+ "	Você recebe menos que um colega de trabalho homem na mesma função?\n\nDigite: \n\n(1) para sim\n\n(2) para não\n\n"));
+	check2 = Integer.parseInt(JOptionPane.showInputDialog("A quantidade de homens que trabalham com você, é maior do que a quantidade de mulheres?\n\nDigite: \n\n(1) para sim\n\n(2) para não\n\n"));
 	if(check2==1) {
-		salario=true;
+		maisHomens=true;
 	}else {
-		salario=false;} 
+		maisHomens=false;} 
+	
+	JOptionPane.showMessageDialog(null, "Em 2019, a taxa de participação das mulheres com 15 anos ou maior de\r\n"
+			+ "idade foi de 54,5%, enquanto entre os homens esta media chegou a 73,7%, uma diferença de 19,2 pontos percentuais. - IBGE 2019\r\n");
+	
 	} 
 	while(check2 != 1 && check2 != 2);
 	
 	opiniaoM = JOptionPane.showInputDialog("O que você acha que pode ser feito para minimizar o abuso contra a mulher no local de trabalho? ");
+	
+	do {
+		check3 = Integer.parseInt(JOptionPane.showInputDialog("Você já presenciou algum abuso no seu local de trabalho?\n\nDigite: \n\n(1) para sim\n\n(2) para não\n\n"));
+		if(check3==1) {
+			maisHomens=true;
+		}else {
+			maisHomens=false;} 
+		
+		JOptionPane.showMessageDialog(null, "Quase metade das mulheres já sofreu algum assédio sexual no trabalho, segundo pesquisa do LinkedIn e da consultoria de inovação social Think Eva [...] Entre elas, 15% pediram demissão do trabalho após o assédio. E apenas 5% delas recorrem ao RH das empresas para reportar o caso. [...] a maioria das entrevistadas que já sofreram alguma forma de assédio sexual no ambiente de trabalho são mulheres negras (52%) e que recebem entre dois e seis salários mínimos (49%). Além disso, o Norte (63%) e Centro-Oeste (55%) têm uma concentração de relatos superior às demais regiões.\r\n"
+				+ "Mais de 95% das entrevistadas afirmam saber o que é assédio sexual no trabalho, mas pouco mais de 51% falam com frequência sobre o tema.\r\n");
+		
+		} 
+		while(check2 != 1 && check2 != 2);
 	
 	if (abuso == true) {
 		feedback = JOptionPane.showInputDialog(null, "Algumas atitudes são importantes para fazer cessar o assédio e evitar que ele se propague e se agrave no ambiente de trabalho:\r\n"
